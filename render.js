768ab62ca4cdf6e660b4d08b8dc9308ab1cc473b
Panagiotis <sl45sms@gmail.com>
2018-11-22 15:01:32 +0200

    better render and fix render only the first

--- render.js
+++ render.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var escapeJSON = require('escape-json-node');
 var fs = require('fs');
+var utils = require('formiojs/utils');
+const http = require('http');
+const https = require('https');
+
 const uuidv1 = require('uuid/v1');
 const RenderPDF = require('chrome-headless-render-pdf');
 
@@ -34,20 +38,50 @@ var template=`
 <head>
     <link rel='stylesheet' href='/app/node_modules/bootstrap/dist/css/bootstrap.min.css'>
     <link rel='stylesheet' href='/app/node_modules/formiojs/dist/formio.full.min.css'>
+    <style>
+@media print{		
+	@page {
+         size: A4;
+       }		
+	
+   .btn {
+                display:none!important;
+        }
+   
+    #formio .no-header ul.list-group-striped {
+        display:none;
+    }
+   
+   #formio .formio-component-datetime .flatpickr-input {
+       direction:rtl!important;
+   }
+
+ #formio > div >div.card {
+	      margin-top:10px;
+          page-break-before: avoid;
+          page-break-after: always;
+  }
+}
+    </style>
     <script src='/app/node_modules/formiojs/dist/formio.full.min.js'></script>
     <script type='text/javascript'>
              window.onload = function() {
-             var form_object=JSON.parse(\`--FORM--\`.replace(/\\r?\\n|\\r/g, ''));
-
-             var submission_object=JSON.parse(\`--SUBMISSION--\`.replace(/\\r?\\n|\\r/g, ''));
-
-                console.log(form_object,submission_object);
-
-                 Formio.createForm(document.getElementById('formio'),form_object.form,{ readOnly: true }).then(function(form) {
-                          form.submission =submission_object;
-                              }
-                       );
-                   }; 
+                var token="--TOKEN--"
+                Formio.clearCache();
+                Formio.setBaseUrl('http://formio-api:3001');
+                Formio.setProjectUrl('http://formio-api:3001');
+                Formio.setToken(token);                 
+                var MyForm= new Formio("/form/--FORM--/submission/--SUBMISSION--");
+				MyForm.loadForm().then(function(rform) {
+                 rform.display="form";
+                 Formio.createForm(document.getElementById('formio'),rform,
+                 { readOnly: true, viewAsHtml: true }).then(function(form) {
+                         MyForm.loadSubmission().then(function(submission) {
+                           form.submission=submission;
+                           });  
+                           });
+                });                       
+              };
     </script>
 </head>
 <body>
@@ -56,28 +90,31 @@ var template=`
 </html>
 `;
 
+
+
 function render(sid,fid,res,type){
+
+
 // First authenticate.
    formio.authenticate(config.formio_admin_email, config.formio_admin_password,"/user/login").then(function() {
-      // Create a new form instance.
-      var form = new Form(config.formio_protocol+'://'+config.formio_host+':'+config.formio_port+'/form/'+fid);
-        
-      form.load().then(function(form_object){
-             form_object.form.display="form"; //alway ovveride in case of wizard 
-             form.loadSubmission(sid).then(function(submission){ 
-                       //Write temp file
-                       var JF = escapeJSON(JSON.stringify(form_object).replace(/\r?\n|\r/g, " "));
-                       var JS = escapeJSON(JSON.stringify(submission).replace(/\r?\n|\r/g, " ")); 
-                       template = template.replace('--FORM--',JF);
-                       template = template.replace('--SUBMISSION--',JS);
+      var jwtToken=formio.currentUser.token;
+
+                      console.log("render form "+fid+" submission "+sid);
+                       
+                       var rtemplate = template.slice(0);
+                       rtemplate = rtemplate.replace('--TOKEN--',jwtToken); 
+                       rtemplate = rtemplate.replace('--FORM--',fid);
+                       rtemplate = rtemplate.replace('--SUBMISSION--',sid);
+                       
+
                        var fn=uuidv1()+'.html';
-                       fs.writeFile("/tmp/"+fn, template, function(err) {
+                       fs.writeFile("/tmp/"+fn, rtemplate, function(err) {
                            if(err) {
                                 res.setHeader('Content-Type', 'application/json');
                                 res.send(err);
                                     }
                                      
-                                    RenderPDF.generatePdfBuffer('file:///tmp/'+fn,{chromeOptions: ['--no-sandbox']}).then((pdfBuffer) => {                                         
+                                    RenderPDF.generatePdfBuffer('file:///tmp/'+fn,{chromeOptions: ['--no-sandbox','--aggressive-cache-discard','--disable-application-cache','--disable-notifications','--disable-remote-fonts','--disable-reading-from-canvas','--disable-reading-from-canvas','--disable-reading-from-canvas','--disable-voice-input','--enable-aggressive-domstorage-flushing']}).then((pdfBuffer) => {                                         
                                       fs.unlinkSync("/tmp/"+fn);
                                       if (type=="pdf"){
                                         res.setHeader('Content-Type', 'application/pdf'); 
@@ -88,11 +125,9 @@ function render(sid,fid,res,type){
                                          res.send('{"file":"' + pdfBuffer.toString('base64') +'"}');
                                         }                                      
                                      
-
                                                });
-                                     });          
-           });
-        });
+                                     });  
+
      });
 }
 
@@ -104,10 +139,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get('/',function(req, res) {
+  render(req.query.submissionid,req.query.formid,res,"pdf");
+});
+
 app.post('/', function(req, res) {
-
   render(req.body.submission._id,req.body.submission.form,res,"pdf");
-
 });
 
 app.post('/base64', function(req, res) {
